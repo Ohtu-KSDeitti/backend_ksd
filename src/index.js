4f63873bc3d99ffb699629996d0d4ac618c6bf6f
k0psutin <mr.gravez@gmail.com>
2021-05-18 12:31:29 +0300

    backend skeleton

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require('apollo-server')
+const { merge } = require('lodash')
+
+/* Esimerkkinä kovakoodattu tietokantataulu ping
+
+Myöhemmin taulujen määritelmät src/models jne?
+*/
+const pingDefs = gql`
+    type Ping {
+        ping: String
+    }
+
+    type Query {
+        pings: [Ping]
+    }
+`
+
+const pings = [
+  {
+    ping: 'pong',
+  },
+]
+
+const pingRes = {
+  Query: {
+    pings: () => pings,
+  },
+}
+//
+
+const server = new ApolloServer({
+  typeDefs: [pingDefs],
+  resolvers: merge(pingRes),
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
