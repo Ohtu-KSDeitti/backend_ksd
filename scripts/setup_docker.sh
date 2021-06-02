990ff967ce75e973b06891ccdd8b94b7533ec7cd
Erkka Rahikainen <ergga96@gmail.com>
2021-06-02 15:47:46 +0300

    Make script for .env

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -4,4 +4,5 @@ sudo yum install docker -y
 sudo service docker start
 sudo docker kill $(docker ps -q)
 sudo docker system prune -a -f
+sudo cp /home/ec2-user/.env /home/ec2-user/user-api
 sudo docker build /home/ec2-user/user-api/ -t user-api
