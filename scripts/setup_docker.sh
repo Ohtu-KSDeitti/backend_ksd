fa75a704b549dca5f25891cb70470506a1544be5
Erkka Rahikainen <ergga96@gmail.com>
2021-06-02 14:00:51 +0300

    Modify docker setup script

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
 sudo docker system prune -a -f
-sudo docker build user-api/ -t user-api
+sudo docker build user-api -t user-api
