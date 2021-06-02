1239ce4d69a2827d0d6428c355dbdf6b6fa586f4
Erkka Rahikainen <ergga96@gmail.com>
2021-06-02 14:41:22 +0300

    Fix docker scripts

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -2,5 +2,6 @@
 sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
+sudo docker kill $(docker ps -q)
 sudo docker system prune -a -f
 sudo docker build /home/ec2-user/user-api/ -t user-api
