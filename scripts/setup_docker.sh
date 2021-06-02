f5d5dc1140545a44c92aa8bc5f03e6c5ed702281
Erkka Rahikainen <ergga96@gmail.com>
2021-06-02 14:08:29 +0300

    Modify setup_docker.sh

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-pwd
-ls
 sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
 sudo docker system prune -a -f
-sudo docker build user-api -t user-api
+sudo docker build /home/ec2-user/user-api/ -t user-api
