3709e4b43b0a87b42bd5c77eb0b87ae6c40e566c
k0psutin <mr.gravez@gmail.com>
2021-06-04 09:37:03 +0300

    Add applyMiddleware

--- src/config/apollo_config.js
+++ src/config/apollo_config.js
@@ -1,51 +1,45 @@
 const { buildFederatedSchema } = require('@apollo/federation')
-// const { findUserById } = require('../models/users')
-// const { docClient } = require('./dynamodb_config')
+const { applyMiddleware } = require('graphql-middleware')
+const { findUserById } = require('../models/users')
+const { docClient } = require('./dynamodb_config')
 const { typeDefs } = require('../models/userTypes')
 const { getResolvers } = require('../models/userResolvers')
-// const jwt = require('jsonwebtoken')
-// const { AuthenticationError } = require('apollo-server')
+const { permissions } = require('../permissions')
+const jwt = require('jsonwebtoken')
 
 const resolvers = getResolvers()
 
 require('dotenv').config()
 
 const PORT = process.env.PORT
-// const JWT_SECRET = process.env.SECRET_KEY
+const JWT_SECRET = process.env.SECRET_KEY
 const ENV = process.env.NODE_ENV
+
 const config = {
-  schema: buildFederatedSchema([
-    {
-      typeDefs,
-      resolvers,
-    },
-  ]), /* ,  context: async ({ req }) => { // Move to gateway-api ?
-    const auth = req.body.variables.authorization || null
-    const login = (req.body.operationName === 'login' ||
-    req.body.operationName === 'addUser')
+  schema: applyMiddleware(
+    buildFederatedSchema([{ typeDefs, resolvers }]),
+    permissions),
+  context: async ({ req }) => {
+    const auth = req.headers.authorization
 
     if (auth && auth.toLowerCase().startsWith('bearer')) {
       const decodedToken = jwt.verify(
         auth.substring(7), JWT_SECRET,
       )
-      console.log(decodedToken.id)
+
       const currentUser = await findUserById(decodedToken.id, docClient)
-      if (!currentUser) {
-        throw new AuthenticationError('User not logged in!')
-      }
       console.log(currentUser)
+
       return { currentUser }
     }
-    if (!login) {
-      throw new AuthenticationError('Unauthorized token!')
-    }
-  } */
+  },
   plugins: [
     {
       requestDidStart: ( requestContext ) => {
         if ( requestContext.request.http?.headers.has( 'x-apollo-tracing' ) ) {
           return
         }
+
         if (ENV !== 'production') {
           const query =
         requestContext.request.query?.replace( /\s+/g, ' ' ).trim()
