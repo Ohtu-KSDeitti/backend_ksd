b342396c59deec2f7fb913f548f82bea72c369bf
k0psutin <mr.gravez@gmail.com>
2021-06-04 09:37:42 +0300

    Add permissions

---
+++ src/permissions/index.js
@@ -0,0 +1,28 @@
+const { rule, shield, and, not } = require('graphql-shield')
+
+const isAuthenticated = rule()((_parent, _args, { currentUser }) => {
+  return Boolean(currentUser)
+})
+
+const isReadingOwnAccount = rule()((_parent, { id }, { currentUser }) => {
+  return currentUser.id === id
+})
+
+const permissions = shield({
+  Query: {
+    findUserById: isAuthenticated,
+    findUserByUsername: isAuthenticated,
+    currentUser: isAuthenticated,
+    getAllUsers: isAuthenticated,
+  },
+  Mutation: {
+    login: not(isAuthenticated),
+    addNewUser: not(isAuthenticated),
+    deleteUserById: and(isAuthenticated, isReadingOwnAccount),
+    updateUserInfo: and(isAuthenticated, isReadingOwnAccount),
+  },
+})
+
+module.exports = {
+  permissions,
+}
