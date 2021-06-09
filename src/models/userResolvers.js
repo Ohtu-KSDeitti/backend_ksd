cb9ea00e123e486f8bc72a88ea9cd1b5ab32d3e9
k0psutin <jkopsutin@gmail.com>
2021-06-09 15:11:22 +0300

    Refactor

--- src/models/userResolvers.js
+++ src/models/userResolvers.js
@@ -10,35 +10,33 @@ const {
   updateUserAccount,
 } = require('./users')
 
-const { docClient } = require('../config/dynamodb_config')
-
-const getResolvers = (client = docClient) =>
+const getResolvers = () =>
   (
     {
       Query: {
         getUserCount: (_root, _args) =>
-          getUserCount(client),
+          getUserCount(),
         getAllUsers: (_root, _args) =>
-          getAllUsers(client),
+          getAllUsers(),
         findUserByUsername: (_root, args) =>
-          findUserByUsername(args.username, client),
+          findUserByUsername(args.username),
         findUserById: (_root, args) =>
-          findUserById(args.id, client),
+          findUserById(args.id),
         currentUser: (_root, _args, context) => {
           return context.currentUser
         },
       },
       Mutation: {
         addNewUser: (_root, args) =>
-          addNewUser(args, client),
+          addNewUser(args),
         deleteUserById: (_root, args) =>
-          deleteUserById(args.id, client),
+          deleteUserById(args.id),
         login: (_root, args) =>
-          login(args.username, args.password, client),
+          login(args.username, args.password),
         updateUserInfo: (_root, args) =>
-          updateUserInfo(args, client),
+          updateUserInfo(args),
         updateUserAccount: (_root, args) =>
-          updateUserAccount(args, client),
+          updateUserAccount(args),
       },
     }
   )
